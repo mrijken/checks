d18a166e67ba0dc7c5fb3fb1b40e03a0e59d3e48
Marc Rijken <marc@rijken.org>
2025-10-06 05:40:02 +0000

    .

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"ray-x/lsp_signature.nvim",
-		config = function()
-			require("lsp_signature").setup({
-				bind = true, -- This is mandatory
-				handler_opts = {
-					border = "rounded", -- Options: 'single', 'double', 'shadow', 'none', 'rounded'
-				},
-			})
-		end,
-	},
-	{
 		"folke/noice.nvim",
 		opts = {
 			presets = {
@@ -24,6 +13,35 @@ return {
 			codelens = {
 				enabled = false,
 			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = function(_, opts)
+			local function read_json_servers(path)
+				local f = io.open(path, "r")
+				if not f then
+					return {}
+				end
+				local content = f:read("*a")
+				f:close()
+				local ok, decoded = pcall(vim.fn.json_decode, content)
+				if not ok or type(decoded) ~= "table" then
+					return {}
+				end
+				return decoded.servers or {}
+			end
+
+			local json_path = vim.fn.stdpath("config") .. "/lua/plugins/lsp.json"
+			local servers = read_json_servers(json_path)
+
+			-- merge with any existing ensure_installed
+			-- opts.ensure_installed = vim.tbl_extend("force", opts.ensure_installed or {}, servers)
+			opts.ensure_installed = servers
+		end,
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
 		},
 	},
 }
