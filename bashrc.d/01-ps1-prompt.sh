2e735ddcd4fab3e43f431ab96cbb6a132b6ae3af
Marc Rijken <marc@rijken.org>
2025-11-09 10:53:59 +0000

    fix ps1

--- bashrc.d/01-ps1-prompt.sh
+++ bashrc.d/01-ps1-prompt.sh
@@ -2,125 +2,40 @@
 
 source "/usr/lib/git-core/git-sh-prompt"
 
-ansi_color() {
-    local type="$1"
-    local color="$2"
-
-    # Function to handle different color formats
-    parse_color() {
-        local type="$1" # "38" for foreground, "48" for background
-        local color="$2"
-
-        if [[ "$color" =~ ^[0-9]+$ ]] && [ "$color" -ge 0 ] && [ "$color" -le 255 ]; then
-            # 256-color mode
-            echo "\033[${type};5;${color}m"
-        elif [[ "$color" =~ ^[0-9]+,[0-9]+,[0-9]+$ ]]; then
-            # RGB mode
-            IFS=',' read -r r g b <<<"$color"
-            echo "\033[${type};2;${r};${g};${b}m"
-        else
-            # Named colors (same as basic function)
-            case "$color" in
-            "black") echo "\033[$([ "$type" = "38" ] && echo "30" || echo "40")m" ;;
-            "red") echo "\033[$([ "$type" = "38" ] && echo "31" || echo "41")m" ;;
-            "green") echo "\033[$([ "$type" = "38" ] && echo "32" || echo "42")m" ;;
-            "yellow") echo "\033[$([ "$type" = "38" ] && echo "33" || echo "43")m" ;;
-            "blue") echo "\033[$([ "$type" = "38" ] && echo "34" || echo "44")m" ;;
-            "magenta") echo "\033[$([ "$type" = "38" ] && echo "35" || echo "45")m" ;;
-            "cyan") echo "\033[$([ "$type" = "38" ] && echo "36" || echo "46")m" ;;
-            "white") echo "\033[$([ "$type" = "38" ] && echo "37" || echo "47")m" ;;
-            *) echo "" ;;
-            esac
-        fi
-    }
-
-    if [ "$type" -eq "fg"]; then
-        echo -e "$(parse_color "$color" "38")"
-    fi
-
-    # Parse background color
-    if [ -n "$type" = "bg" ]; then
-        echo -e "$(parse_color "$color" "48")"
-    fi
-}
-
 function ps1_powerline {
     RETCODE=$? # save return code
     NUM_JOBS=$(jobs -rp | wc -l)
     GIT_BRANCH=$(__git_ps1)
 
-    local GREY="\[\e[48;5;240m\]\[\e[38;5;250m\]"
-    local GREY=""
-    local GREY_END="\[\e[48;5;2m\]\[\e[38;5;240m\]"
+    function w() {
+        local text="$1"
+        local fg="$2"
+        local bg="$3"
+        local arrow="$4"
 
-    local GREEN="\[\e[48;5;2m\]\[\e[38;5;255m\]"
-    local GREEN_END="\[\e[48;5;27m\]\[\e[38;5;2m\]"
+        if [ "$arrow" ]; then
+            echo "$(ansi bg-$bg)$(ansi ${fg})$text$(ansi $bg)"
+        else
+            echo "$(ansi bg-$bg)$(ansi ${fg})$text$(ansi $bg)"
+        fi
+    }
 
-    local ORANGE="\[\e[48;5;208m\]\[\e[38;5;255m\]"
-    local ORANGE_END="\[\e[48;5;236m\]\[\e[38;5;208m\]"
-    local ORANGE_RET_END="\[\e[48;5;160m\]\[\e[38;5;208m\]" # when next segment is prompt with return code
-
-    local BLUE="\[\e[48;5;27m\]\[\e[38;5;255m\]"
-    local BLUE_END="\[\e[48;5;208m\]\[\e[38;5;27m\]"     # when next segment is git
-    local BLUE_END_JOBS="\[\e[48;5;93m\]\[\e[38;5;27m\]" # when next segment is jobs
-    local BLUE_END_ALT="\[\e[48;5;236m\]\[\e[38;5;27m\]" # when next segment is prompt
-    local BLUE_END_RET="\[\e[48;5;160m\]\[\e[38;5;27m\]" # when next segment is prompt with return code
-
-    local JOBS="\[\e[48;5;93m\]\[\e[38;5;255m\] ⏎"
-    local JOBS_END="\[\e[48;5;236m\]\[\e[38;5;93m\]"        # when next segment is prompt
-    local JOBS_NO_RET_END="\[\e[48;5;208m\]\[\e[38;5;93m\]" # when next segment is git
-    local JOBS_NO_GIT_END="\[\e[48;5;160m\]\[\e[38;5;93m\]" # when next segment is prompt with return code
-
-    local RET="\[\e[48;5;160m\]\[\e[38;5;255m\]"
-    local RET_END="\[\e[0m\]\[\e[38;5;160m\]\[\e[0m\] "
-
-    local PROMPT="\[\e[48;5;236m\]\[\e[38;5;255m\]"
-    local PROMPT_END="\[\e[0m\]\[\e[38;5;236m\]\[\e[0m\] "
-
-    if [ ! -w "$PWD" ]; then
-        # Current directory is not writable
-        BLUE_END="\[\e[48;5;160m\]\[\e[38;5;27m\]\[\e[38;5;255m\]  \[\e[48;5;208m\]\[\e[38;5;160m\]"
-        BLUE_END_JOBS="\[\e[48;5;160m\]\[\e[38;5;27m\]\[\e[38;5;255m\]  \[\e[48;5;93m\]\[\e[38;5;160m\]"
-        BLUE_END_ALT="\[\e[48;5;160m\]\[\e[38;5;27m\]\[\e[38;5;255m\]  \[\e[48;5;236m\]\[\e[38;5;160m\]"
-        BLUE_END_RET="\[\e[48;5;160m\]\[\e[38;5;27m\]\[\e[38;5;255m\]  "
-    fi
-
-    TIME_START="$GREY"
-    TIME_END="$END_GREY"
-    HOST_START="$GREEN"
-    HOST_END="$END_GREEN"
-    VIRTUALENV_START="$GREY"
-    VIRTUALENV_END="$END_GREY"
-
-    PS1_PROMPT="$TIMES_START \t $TIME_END"
-
-    PS1_PROMPT="$PS1_PROMPT$HOST_START @\h $HOST_END"
+    PS1_PROMPT="$(w \\t gray black)"
+    PS1_PROMPT="$PS1_PROMPT$(w \\u@\\h white green true)"
 
     if [ "$VIRTUAL_ENV_PROMPT" ]; then
-        PS1_PROMPT="$PS1_PROMPT $VIRTUALENV_START $VIRTUAL_ENV_PROMPT $VIRTUALENV_END"
+        PS1_PROMPT="$PS1_PROMPT$(w $VIRTUAL_ENV_PROMPT white blue true)"
     fi
 
-    if [ -z "$GIT_BRANCH" ]; then
-        # Is not a git repo
-        if [ "$RETCODE" -eq 0 ]; then
-            if [ "$NUM_JOBS" -eq 0 ]; then
-                # No jobs or ret code
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END_ALT$PROMPT \$ $PROMPT_END"
-            else
-                # no ret code but jobs
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END_JOBS$JOBS$NUM_JOBS $JOBS_END$PROMPT \$ $PROMPT_END"
-            fi
-        else
-            if [ "$NUM_JOBS" -eq 0 ]; then
-                # No jobs but ret code is there
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END_RET$RET \$ ⚑ $RETCODE $RET_END"
-            else
-                # Both jobs and ret code
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END_JOBS$JOBS$NUM_JOBS $JOBS_NO_GIT_END$RET \$ ⚑ $RETCODE $RET_END"
-            fi
-        fi
-    else
-        # Is a git repo
+    if [ ! "$RETCODE" -eq -0 ]; then
+        ret_prompt="⚑ $RETCODE"
+        ret_prompt=$(w "$ret_prompt" white red true)
+        PS1_PROMPT="$PS1_PROMPT$ret_prompt"
+    fi
+
+    PS1_PROMPT="$PS1_PROMPT$(w \\W white cyan true)"
+
+    if [ "$GIT_BRANCH" ]; then
         local NUM_MODIFIED=$(git diff --name-only --diff-filter=M | wc -l)
         local NUM_STAGED=$(git diff --staged --name-only --diff-filter=AM | wc -l)
         local NUM_CONFLICT=$(git diff --name-only --diff-filter=U | wc -l)
@@ -131,25 +46,15 @@ function ps1_powerline {
             GIT_STATUS+="\[\e[38;5;255m\]\[\e[48;5;160m\]"
         fi
 
-        if [ "$RETCODE" -eq 0 ]; then
-            if [ "$NUM_JOBS" -eq 0 ]; then
-                # No jobs or ret code
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END$ORANGE $GIT_BRANCH $GIT_STATUS$PROMPT \$ $PROMPT_END"
-            else
-                # no ret code but jobs
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END_JOBS$JOBS$NUM_JOBS $JOBS_NO_RET_END$ORANGE $GIT_BRANCH $GIT_STATUS$PROMPT \$ $PROMPT_END"
-            fi
-        else
-            if [ "$NUM_JOBS" -eq 0 ]; then
-                # No jobs but ret code is there
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END$ORANGE $GIT_BRANCH $GIT_STATUS$RET \$ ⚑ $RETCODE $RET_END"
-            else
-                # Both jobs and ret code
-                PS1="$PS1_PROMPT$GREEN @\h $GREEN_END$BLUE \W $BLUE_END_JOBS$JOBS$NUM_JOBS $JOBS_NO_RET_END$ORANGE $GIT_BRANCH $GIT_STATUS$RET \$ ⚑ $RETCODE $RET_END"
-            fi
-        fi
+        # TODO: add git status
 
+        git_prompt="$GIT_BRANCH"
+        git_prompt=$(w "$git_prompt" white magenta true)
+        PS1_PROMPT="$PS1_PROMPT$git_prompt"
     fi
+
+    PS1_PROMPT="$PS1_PROMPT$(ansi bg-black)$(ansi white)\$ "
+    PS1="$PS1_PROMPT"
 }
 
 if [ "$TERM" != "linux" ]; then
