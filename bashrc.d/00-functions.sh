f0f689ad376173aec2efa78eb0b4578e6edf8869
Rijken, M.A.M.M. (Marc) <marc.rijken@devolksbank.nl>
2025-11-10 08:52:43 +0100

    use tput

--- bashrc.d/00-functions.sh
+++ bashrc.d/00-functions.sh
@@ -1,3 +1,17 @@
+function all_ansi_colors() {
+    for ((i = 0; i < 256; i++)); do
+        echo -n '  '
+        tput setab $i
+        tput setaf $((((i > 231 && i < 244) || ((i < 17) && (i % 8 < 2)) || (\
+        i > 16 && i < 232) && ((i - 16) % 6 * 11 + (i - 16) / \
+        6 % 6 * 14 + (i - 16) / 36 * 10) < 58) ? 7 : 16))
+        printf " %3d " $i
+        tput op
+        ((((i < 16 || i > 231) && ((i + 1) % 8 == 0)) || ((i > 16 && i < 232) && ((i - 15) % 6 == 0)))) &&
+            printf "\n"
+    done
+}
+
 # Colorful logging helper: INFO (green) level
 function info() {
     echo -e "\e[32m* ${*}\e[39m"
--- bashrc.d/01-ps1-prompt.sh
+++ bashrc.d/01-ps1-prompt.sh
@@ -3,14 +3,34 @@
 source "/usr/lib/git-core/git-sh-prompt"
 
 # Default powerline. Set in subsequent script a customized one
-export PS1_PARTS="time user_host venv cwd git_branch git_status retcode"
+export PS1_PARTS="time user_host venv cwd git_branch git_status prompt_sign retcode"
+
+# TODO: use theming
 
 function ps1_powerline {
     RETCODE=$? # save return code
     NUM_JOBS=$(jobs -rp | wc -l)
     GIT_BRANCH=$(__git_ps1)
-
-    # TODO: use tput in stead of ansi
+    normal_fg="15"
+    normal_bg="0"
+    prompt_sign_fg="15"
+    prompt_sign_bg="236"
+    time_fg="0"
+    time_bg="8"
+    user_host_fg="15"
+    user_host_bg="2"
+    host_fg="15"
+    host_bg="2"
+    user_fg="15"
+    user_bg="2"
+    venv_fg="15"
+    venv_bg="214"
+    cwd_fg="0"
+    cwd_bg="110"
+    retcode_fg="15"
+    retcode_bg="1"
+    git_branch_fg="0"
+    git_branch_bg="14"
 
     function w() {
         local text="$1"
@@ -19,9 +39,9 @@ function ps1_powerline {
         local arrow="$4"
 
         if [ "$arrow" ]; then
-            echo "\[$(ansi bg-$bg)\]\[$(ansi ${fg})\]$text\[$(ansi $bg)"
+            echo "\[$(tput setab $bg)\]\[$(tput setaf ${fg})\]$text\[$(tput setaf $bg)\]"
         else
-            echo "\[$(ansi bg-$bg)\]\[$(ansi ${fg})\]$text\[$(ansi $bg)\]"
+            echo "\[$(tput setab $bg)\]\[$(tput setaf ${fg})\]$text\[$(tput setaf $bg)\]"
         fi
     }
 
@@ -31,42 +51,42 @@ function ps1_powerline {
     for PART in $PS1_PARTS; do
         case $PART in
         time)
-            PS1_PROMPT="$PS1_PROMPT$(w \\t gray black $SHOW_ARROW)"
+            PS1_PROMPT="$PS1_PROMPT$(w \\t $time_fg $time_bg $SHOW_ARROW)"
             ;;
 
         user_host)
-            PS1_PROMPT="$PS1_PROMPT$(w \\u@\\h white green $SHOW_ARROW)"
+            PS1_PROMPT="$PS1_PROMPT$(w \\u@\\h $user_host_fg $user_host_bg $SHOW_ARROW)"
             ;;
 
         host)
-            PS1_PROMPT="$PS1_PROMPT$(w \\h white green $SHOW_ARROW)"
+            PS1_PROMPT="$PS1_PROMPT$(w \\h $host_fg $host_bg $SHOW_ARROW)"
             ;;
 
         user)
-            PS1_PROMPT="$PS1_PROMPT$(w \\u white green $SHOW_ARROW)"
+            PS1_PROMPT="$PS1_PROMPT$(w \\u $user_fg $user_bg $SHOW_ARROW)"
             ;;
 
         venv_prompt)
             if [ "$VIRTUAL_ENV_PROMPT" ]; then
-                PS1_PROMPT="$PS1_PROMPT$(w $VIRTUAL_ENV_PROMPT white blue true)"
+                PS1_PROMPT="$PS1_PROMPT$(w $VIRTUAL_ENV_PROMPT $venv_fg $venv_bg true)"
             fi
             ;;
 
         venv)
             if [ "$VIRTUAL_ENV" ]; then
                 venv_basename=$(basename $VIRTUAL_ENV)
-                PS1_PROMPT="$PS1_PROMPT$(w $venv_basename white blue true)"
+                PS1_PROMPT="$PS1_PROMPT$(w $venv_basename $venv_fg $venv_bg true)"
             fi
             ;;
 
         cwd)
-            PS1_PROMPT="$PS1_PROMPT$(w \\W white cyan true)"
+            PS1_PROMPT="$PS1_PROMPT$(w \\W $cwd_fg $cwd_bg true)"
             ;;
 
         retcode)
             if [ ! "$RETCODE" -eq -0 ]; then
                 ret_prompt="⚑ $RETCODE"
-                ret_prompt=$(w "$ret_prompt" white red true)
+                ret_prompt=$(w "$ret_prompt" $retcode_fg $retcode_bg true)
                 PS1_PROMPT="$PS1_PROMPT$ret_prompt"
             fi
             ;;
@@ -74,7 +94,7 @@ function ps1_powerline {
         git_branch)
             if [ "$GIT_BRANCH" ]; then
                 git_prompt="$GIT_BRANCH"
-                git_prompt=$(w "$git_prompt" white magenta true)
+                git_prompt=$(w "$git_prompt" $git_branch_fg $git_branch_bg true)
                 PS1_PROMPT="$PS1_PROMPT$git_prompt"
             fi
             ;;
@@ -95,12 +115,15 @@ function ps1_powerline {
             fi
             ;;
 
+        prompt_sign)
+            PS1_PROMPT="$PS1_PROMPT$(w " \\$ " $prompt_sign_fg $prompt_sign_bg $SHOW_ARROW)"
+            ;;
         esac
 
         SHOW_ARROW="true"
     done
 
-    PS1_PROMPT="$PS1_PROMPT\[$(ansi bg-black)\]\[$(ansi white)\]\$ "
+    PS1_PROMPT="$PS1_PROMPT\[$(tput setab $normal_bg)\]\[$(tput setaf $normal_fg)\]"
     PS1="$PS1_PROMPT"
 }
 
