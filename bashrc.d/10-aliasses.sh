8687277eaeda328509230ab91669b4329c34c62f
Rijken, M.A.M.M. (Marc) <marc.rijken@devolksbank.nl>
2025-12-03 09:07:34 +0100

    feat: rename command

--- bashrc.d/10-aliasses.sh
+++ bashrc.d/10-aliasses.sh
@@ -35,4 +35,4 @@ alias egrep='egrep --color=auto'
 which notify-send &>/dev/null && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # use f to fuzzy search for a git directory and cd to it
-alias f='cd $(fd --type dir --hidden '^\.git$' ~/repos |  sed -e "s/\.git\///" | fzf)'
+alias fcd='cd $(fd --type dir --hidden '^\.git$' ~/repos | sed -e "s/\.git\///" | fzf)'
