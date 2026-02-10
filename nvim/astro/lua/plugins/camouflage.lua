f151ef74257b69daaa1612cbbe96c962a7b17e47
Rijken, M.A.M.M. (Marc) <marc.rijken@asnbank.nl>
2026-02-10 13:05:16 +0100

    feat: add coumaflage

---
+++ nvim/astro/lua/plugins/camouflage.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "zeybek/camouflage.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "<leader>ct", "<cmd>CamouflageToggle<cr>", desc = "Toggle Camouflage" },
+    },
+  },
+}
