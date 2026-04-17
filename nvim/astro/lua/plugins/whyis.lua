e812d0aade14034a0a274cfa67c0a047ae7da30a
Marc Rijken <marc@rijken.org>
2026-04-17 12:06:05 +0000

    add whyis

---
+++ nvim/astro/lua/plugins/whyis.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"takeshid/whyis.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>wf", "<cmd>Whyis float<cr>", desc = "Whyis floating window" },
+			{ "<leader>wr", "<cmd>Whyis right<cr>", desc = "Whyis right side" },
+		},
+	},
+}
